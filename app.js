a90fafd3c44bd551c62dac73acd537778c577917
divyanshchandna <divyanshchandna@gmail.com>
2021-09-22 23:51:13 +0530

    created app.js
    The output is not being printed still but committing to save my work.

---
+++ app.js
@@ -0,0 +1,26 @@
+var btntranslate=document.querySelector("#btn-translate");
+var inputtext=document.querySelector("#input-text");
+var output=document.querySelector("#output");
+
+var serverurl="https://api.funtranslations.com/translate/minion.json";
+function getURL(text){
+    return serverurl+"?"+"text="+text
+}
+
+function clickhandler()
+{
+    var input1=input-text.value;
+    fetch(getURL(input1))
+    .then(response=>response.json)
+    .then(json=>{
+        var translatedtxt=json.contents.translated;
+        output.innertext=translatedtxt;
+    })
+    .catch(errorHandler)
+};
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+btntranslate.addEventListener("click",clickhandler)
